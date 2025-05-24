c65f04686f6220c92967879a41884e2ae1ac066a
Rodrigo Borba <rodrigo14.84@gmail.com>
2025-05-24 03:16:44 +0000

    added new sets and nova enchants removal

--- kubejs/startup_scripts/main.js
+++ kubejs/startup_scripts/main.js
@@ -21,21 +21,158 @@ ItemEvents.modification(event => {
 const fragmentTypes = ['Core', 'Cracked', 'Shard']
 
 
+function applyModifiers(event, itemId, slot, attributes) {
+  const modifiers = attributes.reduce((mod, attr) => {
+    return mod.withModifierAdded(attr.attribute, {
+      amount: attr.amount,
+      id: attr.id,
+      operation: attr.operation,
+    }, slot);
+  }, Item.of(itemId).attributeModifiers);
+
+  event.modify(itemId, item => {
+    item.setAttributeModifiersWithTooltip(modifiers.modifiers());
+  });
+}
+
 ItemEvents.modification(event => {
   global.EYES.forEach(eye => {
     event.modify(eye.id, item => {
       item.rarity = 'EPIC'
     });
   });
-  /*event.modify('minecraft:leather_helmet',item=>{
-    let modifiedAttr = Item.of(item.item().id).attributeModifiers.withModifierAdded(
-      'generic.armor',
-      { amount:50, id:'minecraft:armor.helmet', operation:'add_value'},
-      'head'
-    );
-    //apply
-    item.setAttributeModifiersWithTooltip(modifiedAttr.modifiers())
-  })*/
+
+  // Head
+  applyModifiers(event, 'armoroftheages:holy_armor_head', 'head', [
+    { attribute: 'generic.armor', amount: 1, id: 'end:holy_head_armor_bonus', operation: 'add_value' },
+    { attribute: 'generic.armor_toughness', amount: 4, id: 'end:holy_head_toughness_bonus', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:japanese_light_armor_head', 'head', [
+    { attribute: 'generic.attack_damage', amount: 1, id: 'end:jap_head_attack', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:centurion_armor_head', 'head', [
+    { attribute: 'apothic_attributes:crit_chance', amount: 0.05, id: 'end:cen_head_crit', operation: 'add_value' },
+    { attribute: 'apothic_attributes:crit_damage', amount: 0.1, id: 'end:cen_head_critdamage', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:quetzalcoatl_armor_head', 'head', [
+    { attribute: 'generic.max_health', amount: 0.05, id: 'end:q_head_maxh', operation: 'add_multiplied_base' },
+    { attribute: 'generic.movement_speed', amount: 0.05, id: 'end:q_head_move', operation: 'add_multiplied_base' },
+  ]);
+  applyModifiers(event, 'armoroftheages:anubis_armor_head', 'head', [
+    { attribute: 'generic.armor', amount: 3, id: 'end:anubis_head_armor_bonus', operation: 'add_value' },
+    { attribute: 'generic.armor_toughness', amount: 3, id: 'end:anubis_head_toughness_bonus', operation: 'add_value' },
+    { attribute: 'generic.attack_damage', amount: 2, id: 'end:anubis_head_attack', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:bamboo_hat', 'head', [
+    { attribute: 'combat_roll:count', amount: 1, id: 'end:bamboo_hat1', operation: 'add_value' },
+    { attribute: 'combat_roll:recharge', amount: 0.3, id: 'end:bamboo_hat2', operation: 'add_multiplied_base' },
+  ]);
+  applyModifiers(event, 'cataclysm:bone_reptile_helmet', 'head', [
+    { attribute: 'generic.attack_damage', amount: 2, id: 'end:bone_head_attack', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'cataclysm:ignitium_helmet', 'head', [
+    { attribute: 'irons_spellbooks:spell_power', amount: 0.1, id: 'end:ignis_head_sp', operation: 'add_multiplied_base' },
+    { attribute: 'irons_spellbooks:max_mana', amount: 150, id: 'end:ignis_head_manap', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'cataclysm:cursium_helmet', 'head', [
+    { attribute: 'apothic_attributes:ghost_health', amount: 2, id: 'end:cursium_head_overh', operation: 'add_value' },
+  ]);
+ 
+
+  // Chest
+  applyModifiers(event, 'armoroftheages:holy_armor_chest', 'chest', [
+    { attribute: 'generic.armor', amount: 0, id: 'end:holy_chest_armor_bonus', operation: 'add_value' },
+    { attribute: 'generic.armor_toughness', amount: 4, id: 'end:holy_chest_toughness_bonus', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:japanese_light_armor_chest', 'chest', [
+    { attribute: 'generic.attack_damage', amount: 1, id: 'end:jap_chest_attack', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:centurion_armor_chest', 'chest', [
+    { attribute: 'apothic_attributes:crit_chance', amount: 0.05, id: 'end:cen_chest_crit', operation: 'add_value' },
+    { attribute: 'apothic_attributes:crit_damage', amount: 0.1, id: 'end:cen_chest_critdamage', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:quetzalcoatl_armor_chest', 'chest', [
+    { attribute: 'generic.max_health', amount: 0.05, id: 'end:cq_che_maxh', operation: 'add_multiplied_base' },
+    { attribute: 'generic.movement_speed', amount: 0.05, id: 'end:q_che_move', operation: 'add_multiplied_base' },
+  ]);
+  applyModifiers(event, 'armoroftheages:anubis_armor_chest', 'chest', [
+    { attribute: 'generic.armor', amount: 4, id: 'end:anubis_chest_armor_bonus', operation: 'add_value' },
+    { attribute: 'generic.armor_toughness', amount: 3, id: 'end:anubis_chest_toughness_bonus', operation: 'add_value' },
+    { attribute: 'generic.attack_damage', amount: 2, id: 'end:anubis_chest_attack', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'cataclysm:bone_reptile_chestplate', 'chest', [
+    { attribute: 'apothic_attributes:crit_chance', amount: 0.15, id: 'end:cen_chest_crit', operation: 'add_value' },
+    { attribute: 'apothic_attributes:crit_damage', amount: 0.50, id: 'end:cen_chest_critdamage', operation: 'add_value' },
+  ]);    
+  applyModifiers(event, 'cataclysm:ignitium_chestplate', 'chest', [
+    { attribute: 'irons_spellbooks:spell_power', amount: 0.1, id: 'end:ignis_chest_sp', operation: 'add_multiplied_base' },
+    { attribute: 'irons_spellbooks:max_mana', amount: 150, id: 'end:ignis_chest_manap', operation: 'add_value' },
+  ]);  
+  applyModifiers(event, 'cataclysm:ignitium_elytra_chestplate', 'chest', [
+    { attribute: 'irons_spellbooks:spell_power', amount: 0.1, id: 'xend:ignis_chest_sp', operation: 'add_multiplied_base' },
+    { attribute: 'irons_spellbooks:max_mana', amount: 150, id: 'xend:ignis_chest_manap', operation: 'add_value' },
+  ]);  
+  applyModifiers(event, 'cataclysm:cursium_chestplate', 'chest', [
+    { attribute: 'generic.max_health', amount: 0.1, id: 'end:cursium_chestplate_maxh', operation: 'add_multiplied_base' },
+  ]);
+
+  // Legs
+  applyModifiers(event, 'armoroftheages:holy_armor_legs', 'legs', [
+    { attribute: 'generic.armor', amount: 0, id: 'end:holy_legs_armor_bonus', operation: 'add_value' },
+    { attribute: 'generic.armor_toughness', amount: 5, id: 'end:holy_legs_toughness_bonus', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:japanese_light_armor_legs', 'legs', [
+    { attribute: 'generic.attack_damage', amount: 1, id: 'end:jap_legs_attack', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:centurion_armor_legs', 'legs', [
+    { attribute: 'apothic_attributes:crit_chance', amount: 0.05, id: 'end:cen_legs_crit', operation: 'add_value' },
+    { attribute: 'apothic_attributes:crit_damage', amount: 0.1, id: 'end:cen_legs_critdamage', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:quetzalcoatl_armor_legs', 'legs', [
+    { attribute: 'generic.max_health', amount: 0.05, id: 'end:q_legs_maxh', operation: 'add_multiplied_base' },
+    { attribute: 'generic.movement_speed', amount: 0.05, id: 'end:q_legs_move', operation: 'add_multiplied_base' },
+  ]);
+  applyModifiers(event, 'armoroftheages:anubis_armor_legs', 'legs', [
+    { attribute: 'generic.armor', amount: 4, id: 'end:anubis_legs_armor_bonus', operation: 'add_value' },
+    { attribute: 'generic.armor_toughness', amount: 3, id: 'end:anubis_legs_toughness_bonus', operation: 'add_value' },
+    { attribute: 'generic.attack_damage', amount: 2, id: 'end:anubis_legs_attack', operation: 'add_value' },
+  ]);  
+  applyModifiers(event, 'cataclysm:ignitium_leggings', 'legs', [
+    { attribute: 'irons_spellbooks:spell_power', amount: 0.1, id: 'end:ignis_legst_sp', operation: 'add_multiplied_base' },
+    { attribute: 'irons_spellbooks:max_mana', amount: 150, id: 'end:ignis_legst_manap', operation: 'add_value' },
+  ]);   
+  applyModifiers(event, 'cataclysm:cursium_leggings', 'legs', [
+    { attribute: 'generic.attack_speed', amount: 0.1, id: 'end:cursium_legs_atksped', operation: 'add_multiplied_base' },
+  ]);
+
+  // Boots
+  applyModifiers(event, 'armoroftheages:holy_armor_feet', 'feet', [
+    { attribute: 'generic.armor', amount: 1, id: 'end:holy_boots_armor_bonus', operation: 'add_value' },
+    { attribute: 'generic.armor_toughness', amount: 4, id: 'end:holy_boots_toughness_bonus', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:japanese_light_armor_feet', 'feet', [
+    { attribute: 'generic.attack_damage', amount: 1, id: 'end:jap_feet_attack', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:centurion_armor_feet', 'feet', [
+    { attribute: 'apothic_attributes:crit_chance', amount: 0.05, id: 'end:cen_feet_crit', operation: 'add_value' },
+    { attribute: 'apothic_attributes:crit_damage', amount: 0.1, id: 'end:cen_feet_critdamage', operation: 'add_value' },
+  ]);
+  applyModifiers(event, 'armoroftheages:quetzalcoatl_armor_feet', 'feet', [
+    { attribute: 'generic.max_health', amount: 0.05, id: 'end:q_feet_maxh', operation: 'add_multiplied_base' },
+    { attribute: 'generic.movement_speed', amount: 0.05, id: 'end:q_feet_move', operation: 'add_multiplied_base' },
+  ]);
+  applyModifiers(event, 'armoroftheages:anubis_armor_feet', 'feet', [
+    { attribute: 'generic.armor', amount: 3, id: 'end:anubis_feet_armor_bonus', operation: 'add_value' },
+    { attribute: 'generic.armor_toughness', amount: 3, id: 'end:anubis_feet_toughness_bonus', operation: 'add_value' },
+    { attribute: 'generic.attack_damage', amount: 2, id: 'end:anubis_feet_attack', operation: 'add_value' },
+  ]);  
+  applyModifiers(event, 'cataclysm:ignitium_boots', 'feet', [
+    { attribute: 'irons_spellbooks:spell_power', amount: 0.1, id: 'end:ignis_feet_sp', operation: 'add_multiplied_base' },
+    { attribute: 'irons_spellbooks:max_mana', amount: 150, id: 'end:ignis_feet_manap', operation: 'add_value' },
+  ]);  
+  applyModifiers(event, 'cataclysm:cursium_boots', 'legs', [
+    { attribute: 'generic.movement_speed', amount: 0.15, id: 'end:cursium_feetsped', operation: 'add_multiplied_base' },
+  ]);  
 });
 
 StartupEvents.registry('item', event => {
